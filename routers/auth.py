104d2283c831e1072469e62e9f34faa0ee44603f
nazlitirek <tireknazlihilal@gmail.com>
2026-04-08 18:24:42 +0300

    react app added

--- routers/auth.py
+++ routers/auth.py
@@ -5,6 +5,7 @@ import models, schemas
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -48,9 +49,9 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 @router.post("/login", response_model=schemas.Token)
-def login(email: str, password: str, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == email).first()
-    if not user or not verify_password(password, user.password):
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.password):
         raise HTTPException(status_code=401, detail="Email veya şifre hatalı")
     
     token = create_access_token({"sub": user.email})
